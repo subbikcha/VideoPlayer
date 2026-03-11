499c7e06d07522a39eb8df0e3b2c68a5d0c52f9b
subbikcha <subbikchakarthikeyan19@gmail.com>
2026-03-11 12:22:28 +0530

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Subbikcha K on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
